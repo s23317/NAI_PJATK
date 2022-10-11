a1b6ffa973cd3ee5fe6b8473cc05d89e8742da06
s23317 <s23317@pjwstk.edu.pl>
2022-10-11 21:46:56 +0200

    all done

--- lab1/cmake-build-debug/cmake_install.cmake
+++ lab1/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/s23317/Documents/Git/NAI/lab1
+# Install script for directory: C:/Users/antek/Desktop/PJATK/GitTemp/Nowy folder/NAI_PJATK/lab1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/s23317/Documents/Git/NAI/lab1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/antek/Desktop/PJATK/GitTemp/Nowy folder/NAI_PJATK/lab1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- lab1/main.cpp
+++ lab1/main.cpp
@@ -5,56 +5,44 @@
 #include <string>
 #include <vector>
 #include <cmath>
-using mojamapa_t = std::map<std::string, std::string>;
-using mojafunkcja_t = std::function<double(std::vector<double>)>;
+
+using mojamapa_t = std::map<std::string, double>;
+using mojafunkcja_t = std::function<double(mojamapa_t)>;
 
 
-//void wypisz(mojamapa_t mapa, mojafunkcja_t fun) {
-//    using namespace std;
-//    for (auto kv : mapa) {
-//        auto [k, v] = kv;
-//        cout << "klucz: " << k << "; wartosc " << fun(v) << endl;
-//    }
-//}
-
-
+void wypisz(mojamapa_t mapa, mojafunkcja_t fun) {
+    using namespace std;
+    cout << fun(mapa) << endl;
+}
 
 int main(int argc, char **argv) {
     using namespace std;
-    vector<double> argumentyf;
-//    transform(argv+1, argv+argc,
-//              back_inserter(argumentyf),
-//              [](auto s){return stod(s);});
 
-    for(int i=2; i<argc; i++) {
-        argumentyf.push_back(stod(argv[i]));
-    }
-
-    for (auto e : argumentyf) {
-        cout<<e<<endl;
-    }
-
-//
-//    map<string, string> mapa = {{"sin", "Jan"}};
+    vector<string> argfirst(argv, argv + argc);
+    vector<double> argumenty;
+    map<string, double> mapa;
     map<string, mojafunkcja_t> formatery;
-//
-//
-//
-    formatery["sin"] = [](vector<double> argumentyf) { return sin(argumentyf[0]); };
-    formatery["add"] = [](vector<double> argumentyf) { return argumentyf[0] + argumentyf[1]; };
-    formatery["mod"] = [](vector<double> argumentyf) { return argumentyf[0] / argumentyf[1]; };
-//
-//
-//
-//
-//    try {
-//        vector<string> argumenty(argv, argv + argc);
-//        auto selected_f = argumenty.at(1);
-//        wypisz(mapa, formatery.at(selected_f));
-//    } catch (std::out_of_range aor) {
-//        cout << "podaj argument. Dostepne to: ";
-//        for (auto [k, v] : formatery) cout << " " << k;
-//        cout << endl;
-//    }
+
+    for (int i = 2; i < argc; i++) {
+        argumenty.push_back(stod(argv[i]));
+    }
+
+    for (int i = 0; i < argumenty.size(); i++) {
+        mapa.insert({{"arg" + to_string(i + 1), argumenty.at(i)}});
+    }
+
+    formatery["sin"] = [](map<string, double> map) { return sin(map.at("arg1") * (3.14159 / 180)); };
+    formatery["add"] = [](map<string, double> map) { return map.at("arg1") + map.at("arg2"); };
+    formatery["mod"] = [](map<string, double> map) { return fmod(map.at("arg1"), map.at("arg2")); };
+
+    try {
+        auto selected_f = argfirst[1];
+        cout << selected_f << endl;
+        wypisz(mapa, formatery.at(selected_f));
+    } catch (std::out_of_range aor) {
+        cout << "podaj argument. Dostepne to: ";
+        for (auto [k, v]: formatery) cout << " " << k;
+        cout << endl;
+    }
     return 0;
 }
