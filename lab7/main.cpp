b0b5a5342eab63ee28dfbb86f3b8a754e26bb84e
s23317 <s23317@pjwstk.edu.pl>
2022-11-30 18:24:42 +0100

    added lab7 -all done

---
+++ lab7/main.cpp
@@ -0,0 +1,23 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    using namespace cv;
+    VideoCapture vc(0);
+    if (!vc.isOpened() ) return 1;
+    cv::Mat frame;
+
+    cv::namedWindow("Window", WINDOW_AUTOSIZE); // Create a window for display.
+    int i = 0;
+    while (waitKey(10) != 27) {
+        vc >> frame;
+        std::cout << "frame info: " << frame.rows << " " << frame.cols << std::endl;
+//        imwrite("frame" + std::to_string(i++) + ".png", frame);
+        cv::flip(frame,frame,1);
+        cv::imshow("Window", frame);
+
+    }
+    return 0;
+}
